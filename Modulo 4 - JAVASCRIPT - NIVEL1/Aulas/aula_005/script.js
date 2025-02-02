3bf69f7651f5d4fdac9103fd2b9998fcbcc739e4
MDayvson <mdayvson@outlook.com>
2025-02-01 23:38:03 -0300

    variaveis em javascript

---
+++ Modulo 4 - JAVASCRIPT - NIVEL1/Aulas/aula_005/script.js
@@ -0,0 +1,7 @@
+// comentario js
+console.log(1)
+console.log(2)
+console.log(3)
+
+/* comentario 
+de duas linhas */
---
+++ Modulo 4 - JAVASCRIPT - NIVEL1/Aulas/aula_006/script.js
@@ -0,0 +1,10 @@
+var nome = "João Ribeiro"
+var idade = 21
+var administrador = false
+
+console.log(nome)
+
+var cidade
+cidade = "Lisboa"
+
+var valor1 = 100, nome2 = "Ana", apelido = "Silva"
