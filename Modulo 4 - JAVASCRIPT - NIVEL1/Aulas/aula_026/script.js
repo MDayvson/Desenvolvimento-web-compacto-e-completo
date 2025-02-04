1062902e25c780548aa1010f819f17b96a9aa209
MDayvson <mdayvson@outlook.com>
2025-02-03 23:01:00 -0300

    metodos

--- Modulo 4 - JAVASCRIPT - NIVEL1/Aulas/aula_026/script.js
+++ Modulo 4 - JAVASCRIPT - NIVEL1/Aulas/aula_026/script.js
@@ -1,2 +1,8 @@
 // NAVEGAR NO DOM
 
+let elemento = null;
+
+elemento = document.documentElement;
+console.log('elemento');
+console.log(typeof (elemento))
+console.log(elemento.lang)
---
+++ Modulo 4 - JAVASCRIPT - NIVEL1/Aulas/aula_027/script.js
@@ -0,0 +1,10 @@
+// console.log(document.getElementById('noticia'))
+
+let e = document.getElementById('titulo')
+// console.log(e)
+
+// console.log(e.classList)
+
+console.log(e.innerHTML)
+console.log(e.innerText)
+console.log(e.textContent)
